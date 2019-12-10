fd7ce8d3b5e1bc60081c8b425d71a13a6a6ba04b
sandro_herbst <sandro.herbst@gmail.com>
2019-12-09 23:19:54 -0300

    useState + useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+
+import axios from 'axios'
+
+/*
+axios
+  .get('https://mymoney-herbst.firebaseio.com/valores.json')
+  .then((res) => {
+    console.log(res.data)
+  })
+
+axios
+  .post('https://mymoney-herbst.firebaseio.com/valores.json', {
+    segundo: 'Herbst'
+  })
+  .then((res) => {
+    console.log(res.data)
+  })
+*/
+
+const url = 'https://mymoney-herbst.firebaseio.com/movimentacoes/2019-08.json'
 
 function App() {
+  const [data, setData] = useState({
+    loading: true,
+    data: {}
+  })
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(res => {
+        setData({
+          loading: false,
+          data: res.data
+        })
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Money</h1>
+      {JSON.stringify(data)}
+      {data.loading && <p>Carregando</p>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
