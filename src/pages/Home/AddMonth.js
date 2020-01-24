edc4fedad5264542bf766af4ced86f0f7e8c43bf
sandro_herbst <sandro.herbst@gmail.com>
2020-01-24 12:42:50 -0300

    Adding a month

--- src/pages/Home/AddMonth.js
+++ src/pages/Home/AddMonth.js
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
+import {Redirect} from 'react-router-dom'
+
+const minAno = 2019
+const maxAno = 2022
 
 const AddMonth = () => {
+    const refYear = useRef()
+    const refMonth = useRef()
+    const [redir, setRedir] = useState('')
+    const years = []
+    const months = []
+    for(let i = minAno; i<= maxAno; i++) {
+      years.push(i)
+    }
+    for(let i = 1; i<= 12; i++) {
+      months.push(i)
+    }
+    const zeroPad = num => {
+      if(num < 10) {
+        return '0' + num
+      }
+      return num
+    }
+    const viewMonth = () => {
+      //console.log('ver mes', refYear.current.value, refMonth.current.value)
+      setRedir(refYear.current.value + '-' + refMonth.current.value)
+    }
+    if(redir !== '') {
+      return <Redirect to={`/movements/${redir}`} />
+    }    
     return (
         <>
         <h2>Adicionar Mês</h2>
 
-        <select name="year" id="year">
-          <option value="2019">2019</option>
-          <option value="2020">2020</option>
+        <select ref={refYear} name="year" id="year">
+          {
+            years.map(year => <option key={year} value={year}>{year}</option>)
+          }
         </select>
         
-        <select name="month" id="month">
-          <option value="01">01</option>
-          <option value="02">02</option>
+        <select ref={refMonth} name="month" id="month">
+          {
+            months
+              .map(zeroPad)
+              .map(month => <option key={month} value={month}>{month}</option>)
+          }
         </select>
 
-        <button>Adicionar Mês</button>        
+        <button onClick={viewMonth}>Adicionar Mês</button>        
         </>
     )
 }
